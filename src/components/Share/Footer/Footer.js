cd67acb72ea5aa6362365b3dcbe9556eeb301451
masudrana5 <abdullainfo30@gmail.com>
2022-11-10 15:15:08 +0600

    meaningful footer create somthing ok

--- src/components/Share/Footer/Footer.js
+++ src/components/Share/Footer/Footer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FaClock } from 'react-icons/fa';
+import { TiLocation, TiPhone } from 'react-icons/ti'
+import {GrContact} from 'react-icons/gr'
 
 const Footer = () => {
    return (
@@ -6,21 +9,17 @@ const Footer = () => {
          <footer className="footer p-10 text-base-content container mx-auto">
             <div>
                <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-               <p>Clean East<br />Providing reliable tech since 2022</p>
+               <p>Clean East <span dangerouslySetInnerHTML={{ "__html": "&copy;" }} /> 2022 | All Rights</p>
             </div>
             <div>
-               <span className="footer-title">Services</span>
-               <a className="link link-hover">Commercial Clean</a>
-               <a className="link link-hover">Residensial Clean</a>
-               <a className="link link-hover">Window Clean</a>
-               <a className="link link-hover">Curtain Clean</a>
+               <span className="footer-title text-xl">Contact Me</span>
+               <p className="hover:translate-x-2 duration-500 ml-[-25px]"><TiLocation className='text-2xl inline'></TiLocation> Godhora, Kadimchilan 6430, Lalpur, Natore</p>
+               <p className="hover:translate-x-2 duration-500 ml-[-25px]"><TiPhone className='text-2xl inline'></TiPhone> +880 179 6682951</p>
             </div>
             <div>
-               <span className="footer-title">Company</span>
-               <a className="link link-hover">About us</a>
-               <a className="link link-hover">Contact</a>
-               <a className="link link-hover">Jobs</a>
-               <a className="link link-hover">Press kit</a>
+               <span className="footer-title text-xl mb-8"></span>
+               <p className="hover:translate-x-2 duration-500 ml-[-25px]"><FaClock className='text-xl inline mr-1'></FaClock> Mon-Fri: 9:00 am  5:00 pm <br /> <span className='ml-7'>Sat-Sun: 11:00 am 16:00 pm</span></p>
+               <p className="hover:translate-x-2 duration-500 ml-[-25px]"><GrContact className='text-2xl inline'></GrContact> abdullainfo30@gmail.com</p>
             </div>
             <div>
                <span className="footer-title">Legal</span>
--- src/components/Share/Gallary/ImageGallary.js
+++ src/components/Share/Gallary/ImageGallary.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { IoMdNotificationsOutline } from 'react-icons/io';
 import { PhotoProvider, PhotoView } from 'react-photo-view';
 import 'react-photo-view/dist/react-photo-view.css';
+import image1 from '../../../assets/images/banner-calendar-img.png'
 
 const ImageGallary = () => {
    const images = [
@@ -24,21 +27,44 @@ const ImageGallary = () => {
          image: 'https://i.ibb.co/CnYbDt6/service-page-9-img.jpg',
          id: 5
       },
+      {
+         image: 'https://i.ibb.co/mDCs38y/service-page-8-img.jpg',
+         id: 5
+      },
    ]
    return (
       <section className="py-6  dark:text-gray-50">
-         
-            <PhotoProvider>
-               <div className='grid grid-cols-3 gap-5 container mx-auto'>
-                  {images.map((item, index) => (
-                     <PhotoView key={index} src={item.image}>
-                        <img src={item.image} alt="" />
-                     </PhotoView>
-                  ))}
+         <div className='text-center mb-10' data-aos="fade-down" data-aos-duration="2000">
+            <h1 className='text-5xl text-gray-500 font-semibold mb-8'>Our Projects</h1>
+            <div className='divider border-1 mb-8 w-2/5 mx-auto'><GoPrimitiveDot className='text-gray-500'></GoPrimitiveDot></div>
+         </div>
+         <PhotoProvider
+            speed={() => 800}
+            easing={(type) => (type === 2 ? 'cubic-bezier(0.36, 0, 0.66, -0.56)' : 'cubic-bezier(0.34, 1.56, 0.64, 1)')}
+            loop={6}
+         >
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 container mx-auto p-2'>
+               {images.map((item, index) => (
+                  <PhotoView key={index} src={item.image}>
+                     <img className='hover:scale-105 duration-500 rounded-lg' src={item.image} style={{ objectFit: 'cover' }} alt="" />
+                  </PhotoView>
+               ))}
+            </div>
+         </PhotoProvider>
+         <div className='flex flex-col gap-10 lg:flex-row items-center bg-emerald-400 justify-center mt-10' data-aos="fade-up" data-duration="2000">
+            <img className='mt-[-10px]' src={image1} alt="" />
+            <div className='text-right'>
+               <h1 className='text-4xl text-white mb-[-20px] pb-0'>Save Time. Save Money.</h1><br />
+               <span className='font-bold text-4xl text-white '>Save Yourself!</span>
+               <p className='font-medium text-xl text-white'>Life's Too Short to Clean Your Own Home</p>
+            </div>
+            <div className='flex my-5 lg:mb-5 ml-5'>
+               <div>
+                  <button className="btn rounded-full bg-slate-100  hover:text-white text-black font-semibold duration-500 uppercase ml-5"> <IoMdNotificationsOutline className='mr-1 font-normal text-xl'></IoMdNotificationsOutline> get an estimate</button>
                </div>
-            </PhotoProvider>
-         
-      </section>
+            </div>
+         </div>
+      </section >
    );
 };
 
